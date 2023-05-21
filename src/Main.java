5a60768ba1de392c233eb24f77ec607bb1b055b7
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-05-21 10:28:28 -0300

    Classe de usuário e usuários com Singleton

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Usuarios usuarios = Usuarios.getInstance();
+		Usuario user1 = usuarios.adicionaUsuario("Lucas", "123", "123", "Gerente");
+		Usuario user2 = usuarios.adicionaUsuario("Andreza", "321", "321", "Gerente");
+		Usuario user3 = usuarios.adicionaUsuario("José", "456", "456", "Vendedor");
+		
+		System.out.println(user1.getNome());
+		System.out.println(user1.getMatricula());
+		System.out.println(user2.getMatricula());
+		System.out.println(Arrays.toString(usuarios.listarUsuarios().toArray()));
+		usuarios.removeUsuario("456");
+		System.out.println(Arrays.toString(usuarios.listarUsuarios().toArray()));
+		usuarios = Usuarios.getInstance();
+		System.out.println(Arrays.toString(usuarios.listarUsuarios().toArray()));
 
 	}
 
---
+++ src/Usuarios.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Usuarios {
+	private static Usuarios instance;
+	private List<Usuario> usuarios; 
+	
+	public Usuarios() {
+		this.usuarios = new ArrayList<>();
+	}
+	
+	public Usuario adicionaUsuario(String nome, String matricula, String senha, String tipo) {
+		Usuario user = new Usuario(nome, matricula, senha, tipo);
+		usuarios.add(user);
+		System.out.println("Usuário cadastrado com sucesso!");
+		return user;
+	}
+	
+	public int indexOf(String matricula) {
+		for(int i = 0; i < usuarios.size(); i++) {
+			if(usuarios.get(i).getMatricula().equals(matricula)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void removeUsuario(String matricula) {
+		int index = this.indexOf(matricula);
+		if(index != -1) {
+			usuarios.remove(index);
+			System.out.println("Usuário removido com sucesso!");
+		}else {
+			System.out.println("Nenhum usuário encontrado");
+		}
+	}
+	
+	public List<Usuario> listarUsuarios(){
+		return this.usuarios;
+	}
+	
+	public static Usuarios getInstance() {
+		if(instance == null) {
+			instance = new Usuarios();
+		}
+		return instance;
+	}
+}
